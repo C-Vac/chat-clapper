abd52be5fe41b542f6377ed74352501f5198c5ea
C-Vac <c.vaccarogallo@gmail.com>
2025-05-06 21:06:53 -0400

    add dl url

--- script/chat-clapper.user.js
+++ script/chat-clapper.user.js
@@ -11,6 +11,7 @@
 // @sandbox      JavaScript
 // @run-at       document-start
 // @noframes
+// @downloadURL  https://github.com/C-Vac/chat-clapper/raw/refs/heads/dev/script/chat-clapper.user.js
 // @updateURL    https://github.com/C-Vac/chat-clapper/raw/refs/heads/dev/script/chat-clapper.user.js
 // ==/UserScript==
 
